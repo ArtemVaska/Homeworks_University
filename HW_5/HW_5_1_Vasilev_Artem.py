dbe1d82a32d1d267ef94abb678ce0dd986fc111c
Artem Vasilev <artem_vasilev_01@list.ru>
2023-12-14 17:25:28 +0300

    Solve task in HW_5

---
+++ HW_5/HW_5_1_Vasilev_Artem.py
@@ -0,0 +1,40 @@
+"""
+Написать функцию, которая принимает как аргумент алфавит последовательности,
+а возвращает функцию получения статистики встречаемости символов в последовательности
+"""
+
+
+def create_function_symbol_stats(alphabet: str):
+    """
+    Создает функцию для подсчета статистики символов в последовательности по заданному алфавиту.
+
+    :param alphabet: алфавит, заданный в виде строки без пробельных символов (регистр учитывается)
+    :return: функция для подсчета статистики символов в заданной последовательности
+    """
+    alphabet = set(alphabet)
+
+    def calculate_symbol_stats(sequence: str) -> None:
+        """
+        Подсчитывает процентное содержание каждого символа в заданной последовательности.
+
+        :param sequence: последовательность для подсчета символов в ней
+        :return: печатает на экран результат статистической обработки
+        """
+        stats = {key: 0 for key in sorted(alphabet)}
+        symbols = set(sequence)
+
+        if not (symbols <= alphabet):
+            raise Exception("Sequence's symbols not in alphabet")
+
+        print(f'Symbol percent statistic for {sequence[:3]}...:')
+        for symbol in sorted(symbols):
+            stats[symbol] = sequence.count(symbol)
+        for symbol, stat in stats.items():
+            print(f'{symbol}: {stat / len(sequence):.2%}')
+        print()
+    return calculate_symbol_stats
+
+
+count_nucleotides_DNA = create_function_symbol_stats('ATGCatgc')
+count_nucleotides_DNA('aaaaaattggccatATGAGCTACGATCGACTaaatttggacatttatg')
+# count_nucleotides_DNA('yaATTggC')  # Error
